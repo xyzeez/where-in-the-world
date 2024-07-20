4c743ffa747c37680c1da71302f70b59a137f8f5
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-20 16:50:47 +0100

    🎨 ui(app): added pages

---
+++ src/pages/Countries.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.main``;
+
+const Countries = () => {
+  return <Container>Countr ies</Container>;
+};
+
+export default Countries;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.main``;
+
+const Home = () => {
+  return <Container>home</Container>;
+};
+
+export default Home;
