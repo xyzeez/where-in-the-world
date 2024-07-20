97869c59d3e21a499455efae2232eaac1f5ba604
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-20 16:55:12 +0100

    ✨ feat(app): defined app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+
+// Components
+import AppLayout from './ui/AppLayout';
+
+// Pages
+import Home from './pages/Home';
+import Countries from './pages/Countries';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate replace to="home" />,
+      },
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'countries',
+        element: <Countries />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
