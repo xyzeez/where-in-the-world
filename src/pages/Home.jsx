d61cb2266ad41c8c01abbe5611e11f407013ac40
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-21 20:49:40 +0100

    🎨 ui(app): added home page UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
+import SearchBar from '../ui/SearchBar';
+import FilterBar from '../ui/FilterBar';
+import CountriesList from '../ui/countries/CountriesList';
 
-const Container = styled.main``;
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
 
 const Home = () => {
-  return <Container>home</Container>;
+  return (
+    <Container>
+      <Row>
+        <SearchBar />
+        <FilterBar />
+      </Row>
+      <CountriesList />
+    </Container>
+  );
 };
 
 export default Home;
