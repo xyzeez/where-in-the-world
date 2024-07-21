fd51f5afa005b2f6b8bf32459e07a19868fd3424
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-21 20:44:43 +0100

    🎨 ui(app): added button component

---
+++ src/ui/Button.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 14px;
+  color: var(--theme-text);
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: var(--element-round);
+  box-shadow: var(--element-shadow);
+  background-color: var(--theme-element);
+
+  & > svg {
+    stroke: var(--theme-text);
+    width: 20px;
+    aspect-ratio: 1/1;
+  }
+`;
+
+export default Button;
